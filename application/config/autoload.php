d9e2c1dea27d364512eb1c780425b2c743e4b0e0
muhammadamran <amranhakim9@gmail.com>
2020-08-22 20:50:10 +0700

    Initial commit

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'global_helper','tanggal_helper','form');
+$autoload['helper'] = array('url','form');
 
 /*
 | -------------------------------------------------------------------
